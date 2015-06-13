12ed09269c43b1b5a048e0e5c24c3d6c854d1baa
= <sychris.lee@outlook.com>
2015-06-13 17:29:52 +1000

    opps edit

--- app/controllers/opps_controller.rb
+++ app/controllers/opps_controller.rb
@@ -13,7 +13,33 @@ class OppsController < ApplicationController
     @opp = Opp.new
   end
 
+  # GET /posts/1/edit
+  def edit
+  end
 
+  # PATCH/PUT /opps/1
+  # PATCH/PUT /opps/1.json
+  def update
+    respond_to do |format|
+      if @opp.update(opp_params)
+        format.html { redirect_to @opp, notice: 'Opportunity was successfully updated.' }
+        format.json { render :show, status: :ok, location: @opp }
+      else
+        format.html { render :edit }
+        format.json { render json: @opp.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /opps/1
+  # DELETE /opps/1.json
+  def destroy
+    @opp.destroy
+    respond_to do |format|
+      format.html { redirect_to opps_url, notice: 'Opportunity was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
   def create
     @opp = Opp.new(opp_params)
