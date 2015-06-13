83a57786726cfd6b03a9a018a797c57537f8cf8e
DeveloperAlan <alanng95@hotmail.com>
2015-06-13 16:46:20 +1000

    added add method to organisation

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,2 +1,34 @@
 class OrganisationsController < ApplicationController
+before_action :find_organisation, only: [:show]
+  def new 
+    @organisation = Organisation.new 
+  end 
+
+  def create
+    @organisation = Organisation.new(organisation_params)
+
+    if @organisation.save
+      redirect_to @organisation
+    else 
+      render :new
+    end
+  end
+
+  def show 
+  end 
+
+private 
+
+def find_organisation
+  @organisation = Organisation.find(params[:id])
 end
+
+def organisation_params
+  params.require(:organisation).permit(
+    :name, 
+    :description,
+    :image
+    )
+end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'application#index'
   mount APIS::Base => '/api'
   get 'devise/sessions#destroy', to: 'application#index'
+  resources :organisations
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
