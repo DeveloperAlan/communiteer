3a1e0c30eb9e5cc6ed9b518c1280f55bebfe24af
DeveloperAlan <alanng95@hotmail.com>
2015-06-13 15:53:26 +1000

    hotfix with signout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :volunteers
   root 'application#index'
   mount APIS::Base => '/api'
+  get 'devise/sessions#destroy', to: 'application#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150613044118) do
+ActiveRecord::Schema.define(version: 20150613045216) do
 
   create_table "opps", force: :cascade do |t|
     t.datetime "created_at",  null: false
